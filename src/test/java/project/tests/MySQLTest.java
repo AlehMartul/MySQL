09a8b4466cd901e3de5e3a0c05fb47c41642c7af
Aleh Martul <dark-tower2007@yandex.ru>
2020-07-21 09:21:32 +0300

    last

--- src/test/java/project/tests/MySQLTest.java
+++ src/test/java/project/tests/MySQLTest.java
@@ -7,8 +7,10 @@ import java.sql.SQLException;
 
 public class MySQLTest {
 
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String USER = new ReadPropertyTool(MySQLTest.RESOURCES_PATH, MySQLTest.MYSQL_PROPERTIES)
+            .getProperty("login");
+    private static final String PASSWORD = new ReadPropertyTool(MySQLTest.RESOURCES_PATH, MySQLTest.MYSQL_PROPERTIES)
+            .getProperty("password");
     private static final String DATE = "2015-11-07 21:00:00";
     private static final String BROWSER1 = "firefox";
     private static final String BROWSER2 = "chrome";
